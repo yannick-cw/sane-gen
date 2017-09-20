7d212f27ecf916afb5711d111d18419b2fadbcfb
Davide Cadamuro <davide.cadamuro@gmail.com>
2017-09-20 21:31:35 +0200

    Now it is possible to generate the name of our most loved devOp

--- src/test/scala/io/github/yannick_cw/file_reader/FileReaderSpec.scala
+++ src/test/scala/io/github/yannick_cw/file_reader/FileReaderSpec.scala
@@ -7,9 +7,9 @@ class FileReaderSpec extends FlatSpec with Matchers with Inspectors with OptionV
 
   behavior of "The file reader"
 
-  it should "read all first names" in (FileReader.firstNames should have length 1780)
+  it should "read all first names" in (FileReader.firstNames should have length 1781)
 
-  it should "read all last names" in (FileReader.lastNames should have length 4760)
+  it should "read all last names" in (FileReader.lastNames should have length 4761)
 
   it should "have the first character uppercased and rest lowercased for names" in {
     forAll(FileReader.lastNames ++ FileReader.firstNames) { name =>
